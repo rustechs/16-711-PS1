2c535e70bc8dba3f6c5a23f2745e78b02ffef2b0
thun1 <thun1@andrew.cmu.edu>
2017-01-31 00:39:03 -0500

    Collision

---
+++ sphereCollision.m
@@ -0,0 +1,26 @@
+function [c,ceq]=sphereCollision(q, obstacles)
+ceq=0;
+%obstacles mx4 (x,y,z,r)
+jointCoords=jointPoses(q); %%nx3,gives (x,y,z) coordinate of joint
+A=zeros(size(jointCoords,1),size(obstacles,1));
+jointCoords=[0,0,0;jointCoords];
+for i=2:size(jointCoords,1)
+    for j=1:size(obstacles,1)
+        p=obstacles(j,1:3);
+        v=jointCoords(i-1,:);
+        w=jointCoords(i,:);
+        d=norm(cross(w-v,p-v))/norm(w-v);
+        if d<obstacles(j,4)
+            t =dot(p - v, w - v) / norm(w-v);
+            if t<norm(w-v)
+                A(i,j)=obstacles(j,4)-d;
+            else
+                A(i,j)=obstacles(j,4)-sqrt((t-norm(w-v))^2+d^2);
+            end
+        else
+            A(i,j)=obstacles(j,4)-d;
+                
+        end
+    end
+end
+c=max(A,[],2);
